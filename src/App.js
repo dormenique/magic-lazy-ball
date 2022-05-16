fd05572a25d2e28d701d33eacbc901fa74043182
dormenique <dormenique@gmail.com>
2022-05-16 22:09:48 +0100

    style ball

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  let userQuestion = document.getElementById("input").textContent;
-  console.log(userQuestion);
-
   return (
-    <div className="App">
-      <h1>Magic Lazy Ball</h1>
-      <h3>Hello! Let's see what is up with the future...</h3>
-      <input id="input" type="text" placeholder="type your question here" />
+    <body>
+      <div className="app">
+        <header className="app-header">
+          <h1>Magic Lazy Ball</h1>
+        </header>
+        <h3>Hello! Let's see what is up with the future...</h3>
+        <input
+          className="input"
+          id="input"
+          type="text"
+          placeholder="Insert your question here"
+        />
+        <button>Ask</button>
 
-      <div>
-        <h2>The Magic Lazy Ball answers...</h2>
-        <p></p>
+        <div className="ball-shape">
+          <div className="ball-content">
+            <h3 className="h3-ball">The Magic Lazy Ball answers...</h3>
+            <div className="display-answer">
+              <h4 className="answer-value">insert the input value here</h4>
+            </div>
+          </div>
+          <button className="do-it-again">Do it again</button>
+        </div>
       </div>
-    </div>
+      <footer>
+        Made with Love by <a href="dormenique.com">@dormenique</a>
+      </footer>
+    </body>
   );
 }
 
